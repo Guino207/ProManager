c53562ea16f0bca05244a9c1e35f65e98a80ce52
Guilherme Filipe <guilhermefilipe918@gmail.com>
2026-05-11 00:38:51 +0100

    Esqueleto do cadastro feito!

---
+++ sign.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container" id="container">
+        <div class="insira">
+            <form action="" method="post">
+                <h1>Cadastro</h1>
+                <input type="text" placeholder="Digite o seu nome" name="name">
+                <input type="email" placeholder="Digite o seu email" name="email">
+                <input type="password" placeholder="Digite a sua password" name="senha">
+                <input type="password" placeholder="Confirme a sua password" name="password">
+
+
+                <button type="submit" name="cadastrar">Cadastrar</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
