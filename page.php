10163b7c38d379af3e16502cbcb883984709d3ae
Guilherme Filipe <guilhermefilipe918@gmail.com>
2026-05-16 21:58:25 +0100

    Put new link

--- page.php
+++ page.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ProManager - Sistema de Gerenciamento de Projetos</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="./assets/css/other.css">
+    <link rel="stylesheet" href="./assets/css/page.css">
     <style>
         /* Espaço para o encerramento de sessão imediata */
 
