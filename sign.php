959f79cd3baa4f2ff45357a4c0fa8100477a1854
Guilherme Filipe <guilhermefilipe918@gmail.com>
2026-05-17 21:46:22 +0100

    .

--- sign.php
+++ sign.php
@@ -124,19 +124,13 @@ if(isset($_POST['login'])){
         <div class="toggle-container">
             <div class="toggle">
                 <div class="toggle-painel toggle-left">
-
                     <img src="./assets/Logo.png" alt="logotipo">
                     <p>Crie a sua conta e gerencie os seus projetos com propriedade</p>
                     <button class="hidden" id="login">login</button>
                 </div>
                 <div class="toggle-painel toggle-right">
-                    <img src="./assets/Logo.png" alt="logotipo">
-                    <h1>Bem-Vindo</h1>
-                    <button class="hidden" id="login">login</button>
-                </div>
-                <div class="toggle-painel toggle-right">
-                    <img src="./assets/img/" alt="logotipo" heigth="400px" width="200px">
-                    <h2>Bem vindo ao ProManager</h2>
+                    <img src="./assets/Logo.png" alt="logotipo" heigth="400px" width="200px">
+                    <h2></h2>
 
                     <button class="hidden" id="cadastro">cadastrar</button>
                 </div>
